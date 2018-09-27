47007f854a6733e136368257cf25e96e5df4c760
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-09-26 23:35:09 -0300

    Component New Bill

--- frontend/src/components/Bill/NewBill.js
+++ frontend/src/components/Bill/NewBill.js
@@ -1,20 +1,62 @@
 import React, { Component } from "react";
 import { findAll } from "./API";
-import { Container, Row, Col, Button, Form } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
 class NewBill extends Component {
   state = {
-    bills: {}
+    bill: {
+      name: "",
+      value: "",
+      type: 0
+    },
+    type: ["INPUT", "OUTPUT"]
   };
 
   render() {
-    const { bill } = this.state;
+    const { bill, type } = this.state;
     return (
       <Container>
         <Row>
           <Col xs="12" sm="12" md="12" lg="12">
-            <Form>form</Form>
+            <Form>
+              <FormGroup>
+                <Label>Bill Name:</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="Enter a bill name"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>Bill Value:</Label>
+              </FormGroup>
+              <FormGroup>
+                <Label>Bill Type:</Label>
+                <Input type="select" name="type">
+                  {type.map((type, index) => (
+                    <option value={type} key={index}>
+                      {type}
+                    </option>
+                  ))}
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Button color="primary">Create new Bill</Button>
+                <Link to="/bills" className="btn btn-secondary">
+                  Back to list
+                </Link>
+              </FormGroup>
+            </Form>
           </Col>
         </Row>
       </Container>
